7f8e755a07b83ef6e25f7aceb0f745c849bccbfc
gabymy <66925220+gabymy@users.noreply.github.com>
2021-09-05 14:23:45 -0300

    Add files via upload
    desktop notification

---
+++ notificaciones.py
@@ -0,0 +1,10 @@
+import time
+from plyer import notification
+if __name__=='__main__':
+    while True:
+        notification.notify(
+            title ='Take a class',
+            message = 'I have to take a python workshop',
+            timeout = 10
+            )
+        time.sleep(15)
